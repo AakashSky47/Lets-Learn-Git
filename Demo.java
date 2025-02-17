134652f35c8a74c646aaaf4e510d0c000a908ddb
Aakash Khurange <aakashsecured@gmail.com>
2025-02-17 13:10:53 +0530

    initial commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Welcome");
+        //Addition of two number
+
+        int a=10,b=20,c;
+        c=a+b;
+        System.out.println("Additon of two number:"+c);
+    }
+}
